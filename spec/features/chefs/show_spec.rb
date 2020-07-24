8a45d9613ccd7aee179cb6257a58dcf6efa2a77f
“Neeru <neeram85@gmail.com>
2020-07-24 09:32:51 -0600

    Add routes for dishes

---
+++ spec/features/chefs/show_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "Chef show page" do
+  it "I see the name of the chef and a link to view a list of all ingredients this chef uses in dishes" do
+
+  end
+
+  it "I see the three most popular ingredients that the chef uses in their dishes" do
+
+  end
+end
+
+# As a visitor
+# When I visit a chef's show page
+# I see the name of that chef
+# And I see a link to view a list of all ingredients that this chef uses in their dishes
+# When I click on that link
+# I'm taken to a chef's ingredient index page
+# and I can see a unique list of names of all the ingredients that this chef uses
+
+# As a visitor
+# When I visit a chef's show page
+# I see the three most popular ingredients that the chef uses in their dishes
+# (Popularity is based off of how many dishes use that ingredient)
---
+++ spec/features/dishes/show_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe "Dish show page" do
+  before(:each) do
+    @tony = Chef.create(name: "Anthony Bourdain")
+
+    @sunday_gravy = Dish.create(name: "Sunday Gravy with Sausage and Rigatoni", description: "An Italo-American Jersey classic")
+    @boeuf = Dish.create(name: "Boeuf Bourguignon", description: "A classic French dish that sticks to your ribs")
+
+    @sausage = Ingredient.create(name: "Hot Pork Sausage Links", calories: 240.0)
+    @rigatoni = Ingredient.create(name: "Rigatoni", calories: 340.0)
+    @parm = Ingredient.create(name: "Parmigiano-Reggiano", calories: 400)
+
+    @garlic = Ingredient.create(name: "Garlic Clove", calories: 50.0)
+    @carrots = Ingredient.create(name: "Carrots", calories: 140.0)
+    @beef = Ingredient.create(name: "Beef Shoulder", calories: 450.0)
+  end
+
+  it "I see a list of ingredients for that dish and the chef's name" do
+    visit "/dishes/#{@sunday_gravy.id}"
+
+    expect(page).to have_content(@tony.name)
+    expect(page).to have_content(@sausage.name)
+    expect(page).to have_content(@rigatoni.name)
+    expect(page).to have_content(@parm.name)
+  end
+  #
+  # it "I see the total calorie count for that dish" do
+  #
+  # end
+end
+
+
+
+# As a visitor
+# When I visit a dish's show page
+# I see the total calorie count for that dish.
